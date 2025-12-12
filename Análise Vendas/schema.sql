2ce99368606314cd0be182f0cf4570c1765c4415
Carlos Vinicius <carlosviniciusrcdev@gmail.com>
2025-12-12 00:15:47 -0300

    att

--- Análise Vendas/schema.sql
+++ Análise Vendas/schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE Vendas;
+CREATE DATABASE Empresa;
 
-USE Vendas;
+USE Empresa;
 
 CREATE TABLE clientes(
 	id_cliente VARCHAR(50) PRIMARY KEY NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE clientes(
 );
 
 CREATE TABLE pedidos(
-	id_pedido VARCHAR(30) NOT NULL PRIMARY KEY,
+	id_pedido VARCHAR(50) NOT NULL PRIMARY KEY,
     data_pedido DATE,
 	data_envio DATE,
     modo_envio VARCHAR(50),
@@ -22,17 +22,16 @@ CREATE TABLE pedidos(
 );
 
 CREATE TABLE produtos(
-	id_produto VARCHAR(30) PRIMARY KEY NOT NULL,
+	id_produto VARCHAR(50) PRIMARY KEY NOT NULL,
     categoria VARCHAR(80) NOT NULL,
     subcategoria VARCHAR(30),
     nome_produto VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE vendas(
-	id_venda INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    id_pedido VARCHAR(30) NOT NULL,
+    id_pedido VARCHAR(50) NOT NULL,
     id_cliente VARCHAR(50) NOT NULL,
-    id_produto VARCHAR(30) NOT NULL,
+    id_produto VARCHAR(50) NOT NULL,
     valor_venda DECIMAL (10,2) NOT NULL,
     quantidade_vendida INT NOT NULL,
     custo_envio DECIMAL(10,2),
@@ -40,3 +39,5 @@ CREATE TABLE vendas(
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
     FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
 );
+
+
